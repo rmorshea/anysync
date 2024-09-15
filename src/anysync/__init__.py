6077cce1f81dda4497735472ed5117554cada4ce
Ryan Morshead <ryan.morshead@gmail.com>
2024-09-14 20:50:26 -0600

    v0.3.1 version bump

--- src/anysync/__init__.py
+++ src/anysync/__init__.py
@@ -1,4 +1,4 @@
 from anysync.core import AnySyncContextManager, AnySyncCoroutine, AnySyncGenerator, contextmanager, coroutine, generator
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __all__ = ["AnySyncContextManager", "AnySyncCoroutine", "AnySyncGenerator", "contextmanager", "coroutine", "generator"]
